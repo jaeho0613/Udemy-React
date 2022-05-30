ea3dba514ba9bcc3ae53d867644c41ac41e2e5e9
ywm2004 <ywm2004@lfpartners.co.kr>
2022-05-30 17:53:07 +0900

    Section05. 중간저장

--- section05/src/App.js
+++ section05/src/App.js
@@ -1,37 +1,41 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import {useState} from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id    : "e1",
+    title : "제목1",
+    amount: "100",
+    date  : new Date(2022, 4, 30)
+  },
+  {
+    id    : "e2",
+    title : "제목2",
+    amount: "200",
+    date  : new Date(2022, 4, 31)
+  },
+  {
+    id    : "e3",
+    title : "제목3",
+    amount: "300",
+    date  : new Date(2022, 5, 1)
+  }
+];
 
 function App() {
-  const expenses = [
-    {
-      id    : "e1",
-      title : "제목1",
-      amount: "100",
-      date  : new Date(2022, 4, 30)
-    },
-    {
-      id    : "e2",
-      title : "제목2",
-      amount: "200",
-      date  : new Date(2022, 4, 31)
-    },
-    {
-      id    : "e3",
-      title : "제목3",
-      amount: "300",
-      date  : new Date(2022, 5, 1)
-    }
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    expenses.push(expense);
-    console.log(expenses);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- section05/src/components/Expenses/ExpenseItem.js
+++ section05/src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,19 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
+import {useState} from "react";
 
 const ExpenseItem = (props) => {
-  
-  const [title, setTitle] = useState(props.title);
-  
-  const clickHandler = () => {
-    setTitle("Update!!");
-    console.log(title);
-  };
-  
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{props.amount}원</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date}/>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}원</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
